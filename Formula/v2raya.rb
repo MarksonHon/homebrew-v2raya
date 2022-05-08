d9cc779f5bf3624f1a4e67300e2e01d14520ecd3
markson <markson@2333ma.top>
2022-05-08 12:07:56 +0800

    Try to fix OS check

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -16,12 +16,12 @@ class V2raya < Formula
     system "bash", "-c", "/tmp/1.sh"
     aFile = File.new("/tmp/uname.txt", "r")
    if aFile
-       OS_Kernel = aFile.sysread(20)
+       $OS_Kernel = aFile.sysread(20)
        puts OS_Kernel
    else
        puts "Unable to open file!"
    end
-   if OS_Kernel == "Linux"
+   if $OS_Kernel == "Linux"
        url $url_linux_x64
        sha256 $sha_linux_x64
    elsif Hardware::CPU.intel?
