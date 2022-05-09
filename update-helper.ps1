ea2bdf7d400eabc87409d875fe6068a7f55ab803
Markson Ho <markson@2333ma.top>
2022-05-10 07:50:20 +0800

    Remove upx

--- update-helper.ps1
+++ update-helper.ps1
@@ -9,21 +9,19 @@ Invoke-WebRequest -Uri $v2raya_url -OutFile 'v2raya_latest.tar.gz'
 tar -xzf ./v2raya_latest.tar.gz
 Set-Location $v2raya_source_path
 pwsh -c "Set-Location ./gui ;yarn; yarn build"
-Copy-Item -Path ./web -Destination ./service/server/router/ -Force -Recurse
-Set-Location ./service
-$env:CGO_ENABLED = "0"
-$build_flags = '-X github.com/v2rayA/v2rayA/conf.Version=' + $v2raya_latest + '-homebrew' + ' -s -w'
-$env:GOARCH = "amd64"; $env:GOOS = "linux"; go build -ldflags $build_flags -o '../v2raya_amd64_linux'
-$env:GOARCH = "amd64"; $env:GOOS = "darwin"; go build -ldflags $build_flags -o '../v2raya_amd64_darwin'
-$env:GOARCH = "arm64"; $env:GOOS = "darwin"; go build -ldflags $build_flags -o '../v2raya_arm64_darwin'
-Set-Location ../
 
 New-Item -ItemType Directory -Path ./ -Name "v2raya-x86_64-linux"
 New-Item -ItemType Directory -Path ./ -Name "v2raya-x86_64-macos"
 New-Item -ItemType Directory -Path ./ -Name "v2raya-aarch64-macos"
-upx -9 "./v2raya_amd64_linux" -o "./v2raya-x86_64-linux/v2raya"
-upx -9 "./v2raya_amd64_darwin" -o "./v2raya-x86_64-macos/v2raya"
-upx -9 "./v2raya_arm64_darwin" -o "./v2raya-aarch64-macos/v2raya"
+
+Copy-Item -Path ./web -Destination ./service/server/router/ -Force -Recurse
+Set-Location ./service
+$env:CGO_ENABLED = "0"
+$build_flags = '-X github.com/v2rayA/v2rayA/conf.Version=' + $v2raya_latest + '-homebrew' + ' -s -w'
+$env:GOARCH = "amd64"; $env:GOOS = "linux"; go build -ldflags $build_flags -o '../v2raya-x86_64-linux/v2raya'
+$env:GOARCH = "amd64"; $env:GOOS = "darwin"; go build -ldflags $build_flags -o '../v2raya-x86_64-macos/v2raya'
+$env:GOARCH = "arm64"; $env:GOOS = "darwin"; go build -ldflags $build_flags -o '../v2raya-aarch64-macos/v2raya'
+Set-Location ../
 
 Compress-Archive -Path "./v2raya-x86_64-linux/*" -DestinationPath "../v2raya-x86_64-linux.zip"
 Get-FileHash "../v2raya-x86_64-linux.zip" | Select-Object Hash | ForEach-Object { ([string]$_.Hash) } | Out-File -Path "../v2raya-x86_64-linux-sha256.txt"
