78d4ae1948cef6e460e774851bdee0add1f0f78d
Markson Ho <markson@2333ma.top>
2022-05-10 07:59:06 +0800

    v2rayA: Update to version 1.5.7-10

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -2,13 +2,13 @@ class V2raya < Formula
     desc "Web-based GUI client of Project V"
     homepage "https://v2raya.org"
     license "AGPL-3.0-only"
-    version "1.5.7-9"
+    version "1.5.7-10"
     ## Install v2rayA
-    $url_linux_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-9/v2raya-x86_64-linux.zip"
+    $url_linux_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-10/v2raya-x86_64-linux.zip"
     $sha_linux_x64 = "3BC57DAE8BEBC4BB39C8F8F28C3CABC4E34022F297BAB19CBEC1B8E1EF452FA1"
-    $url_macos_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-9/v2raya-x86_64-macos.zip"
+    $url_macos_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-10/v2raya-x86_64-macos.zip"
     $sha_macos_x64 = "808387E83008C39854856563F09606FB13EA3E9404EF48B35CAE9ECB9280BE5A"
-    $url_macos_arm64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-9/v2raya-aarch64-macos.zip"
+    $url_macos_arm64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-10/v2raya-aarch64-macos.zip"
     $sha_macos_arm64 = "5B3D839ED9854D41F031A2266B99C027975DCBC711FC562BE477DF5E9C29A6F8"
     if OS.linux?
       url $url_linux_x64
