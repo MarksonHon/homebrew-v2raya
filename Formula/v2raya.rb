1a8a1514fb472d2affb5b92a87c43c9574255c70
markson <markson@2333ma.top>
2022-05-10 22:11:31 +0800

    v2rayA: Use Go 1.17 to build

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -2,14 +2,14 @@ class V2raya < Formula
     desc "Web-based GUI client of Project V"
     homepage "https://v2raya.org"
     license "AGPL-3.0-only"
-    version "1.5.7-11"
+    version "1.5.7-13"
     ## Install v2rayA
-    $url_linux_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-11/v2raya-x86_64-linux.zip"
-    $sha_linux_x64 = "49A7A50907E632EB452D540527256C0C8F3EE08DA97048EC3C2CDE1F7E28AC3A"
-    $url_macos_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-11/v2raya-x86_64-macos.zip"
-    $sha_macos_x64 = "47DA937C986684F7777A593F86327F896142A1FE08AD90E0E4444128384224CD"
-    $url_macos_arm64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-11/v2raya-aarch64-macos.zip"
-    $sha_macos_arm64 = "77162C461AF45F0BA36172905D815719AE94ACE8DEF390ACA8FC4CFB96CEBA35"
+    $url_linux_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-13/v2raya-x86_64-linux.zip"
+    $sha_linux_x64 = "D3428F866B33702E2BF5339757B9168BCCB7ACB13F1D08521F7080F152488D5B"
+    $url_macos_x64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-13/v2raya-x86_64-macos.zip"
+    $sha_macos_x64 = "A212BD13A92A2536A153F1D30A55497844FE9ED2E890FB921F811E99BC628BAC"
+    $url_macos_arm64 = "https://github.com/MarksonHon/homebrew-v2raya/releases/download/1.5.7-13/v2raya-aarch64-macos.zip"
+    $sha_macos_arm64 = "CBA6579BDA84F81FAAA961E423F3265C90D71013DED017A2E98BFDF562517AB2"
     if OS.linux?
       url $url_linux_x64
       sha256 $sha_linux_x64
